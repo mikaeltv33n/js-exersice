47c26a23cb2ea7eb86308be69f773abfacf88a0d
mikaeltv33n <mikaeltv33n>
2023-03-10 13:52:50 +0100

    first commit

---
+++ javascript_del1.js
@@ -0,0 +1,37 @@
+//document.querySelector("#opg10").addEventListener("mouseover",function(){
+//document.querySelector("#opg10").className ='normal';
+//});
+
+//document.querySelector("#opg10").addEventListener("mouseout",function(){
+//document.querySelector("#opg10").className ='skift';
+//});
+
+
+//alert("uggubugga");
+// confirm(vil du lave lektier);
+//document.write("hellooo wooorld");
+//document.querySelectorAll("#link").addEventListener("click",function(){
+// alert("hej der");
+
+
+//document.addEventListener("click",function(){
+//alert("hej der");
+
+//});
+
+document.querySelector("#link").addEventListener("click", function () {
+    alert("hej der");
+});
+
+function normal() {
+    document.querySelector('#opg10').className = 'normal'
+}
+
+function skift() {
+    document.querySelector('#opg10').className = 'skift'
+}
+
+function colorChange(color) {
+    document.querySelector('#opg12').style.backgroundColor=color;
+}
+
---
+++ variabler/javascript_del2.js
@@ -0,0 +1,151 @@
+//opg2
+//var tekst = "hej verden"; alert(tekst);
+
+//opg3
+//var start = "hej "; 
+//var slut = "verden";                       
+//var samlet = start + slut;
+//console.log(samlet);
+//document.write(samlet);
+
+//opg4
+//var tal1 = 2; 
+//var tal2 = 5; 
+//var tallet = tal1 + tal2;
+
+//document.write(tallet)
+
+
+//opg5
+//var tekst = "dette er en tekst";
+//var tal = "1337"
+//var samlet = tekst + tal;
+//document.write(samlet)
+
+
+//opg6
+//var tal = 400;
+//var tal2 = 200;
+
+//alert(tal * tal2);
+//alert(tal + tal2);
+//alert(tal - tal2);
+//alert(tal / tal2);
+
+
+//opg7
+//var  tal = prompt (475);
+//document.write(tal);
+
+
+//opg8
+
+    //var tal = prompt ("skriv et tal");
+
+//if(isNaN(tal)){    
+    //var tal = prompt ("skriv et tal eller lad livet");
+
+   
+//}   
+
+//opg9
+
+//var tal = prompt ("skriv et tal");
+
+//if(isNaN(tal)){    
+    //var tal = prompt ("skriv et tal eller lad livet");
+//}   
+//else{
+    //document.write("sådan")
+//}
+
+//opg10
+//var tal = prompt("5"); 
+    //while(isNaN(tal)){    
+    //tal = prompt("400"); 
+
+    //alert(tal + "er et tal") 
+//} 
+
+
+//opg11 
+//var tal = 40;
+//var tekst = "hej med dig"; 
+//Boolean = false;
+
+//alert(typeof(tekst));
+
+
+//opg12
+//var tekst = prompt ("skriv noget")
+//console.log(typeof(tekst));
+
+//opg13
+//var tekst1 = tekst1 = prompt ("skriv noget")
+//console.log(typeof(tekst1));
+
+//var tekst2 = prompt ("skriv noget nu")
+//console.log(typeof(tekst2));
+//var tekstblandet = tekst1 + tekst2;
+//console.log(tekstblandet)
+
+
+//opg14
+//var i = 1; 
+//while(i < 5) {
+
+//console.log("i er mindre end 5");    
+    //i++;
+//} 
+//document.write("du er nu over 5");
+
+//opg15
+//var i = 1;
+//while(i < 11){
+    //console.log("i er mindre end 11");  
+    
+    //if(i % 2 == 0){
+    //console.log("tælle variablens værdi er lige" + i);  
+    //} 
+    //else {
+        //console.log("tælle variablens værdi er ulige" + i);
+    //}
+    //i++;
+//}
+
+//opg16
+//var skriv = prompt("skriv noget nu")
+
+//if(isNaN(skriv)){
+    //alert(skriv +" er ikke et tal")
+//}
+
+//else{
+
+//if(skriv%2==0){
+    //alert(skriv +" er et lige tal")
+//}
+//else{
+    //alert(skriv +" er et ulige tal")
+//}
+
+//}
+
+
+//opg17
+//var i = 1;
+//while(i < 5) {
+   //document.write("<p>her er noget tekst</p>")
+    //i++;
+//} 
+
+//opg18
+
+//document.addEventListener("DOMContentLoaded", function() {    
+    //var i = 1;
+    //while(i < 5) {
+   //console.log(i);
+    //document.querySelector('#awesomeness').innerHTML += "<p>tekst</p>";    
+    //i++; 
+//}        
+//})
